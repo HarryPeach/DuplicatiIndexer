6357d66be3bfd9953f4ff8b85d2df265e140c8c5
Harry Peach (Personal Signing Key) <harrypeach1999@gmail.com>
2021-08-01 23:09:45 +0100

    ✅ Add a basic test for decode_filelist

--- tests/test_jsonpy.py
+++ tests/test_jsonpy.py
@@ -1,5 +1,20 @@
-from jsonpy import __version__
+import ijson
+from expects import expect, equal
+from jsonpy.__main__ import decode_filelist
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+def test_decode_filelist():
+    """Tests the decode_filelist function."""
+    dec_str = decode_filelist('./tests/resources/sample_filelist.json')
+    objects = ijson.items(bytes(dec_str, 'utf-8'), 'item')
+
+    file_count = 0
+    folder_count = 0
+    for obj in objects:
+        if obj["type"] == "File":
+            file_count += 1
+        elif obj["type"] == "Folder":
+            folder_count += 1
+
+    expect(file_count).to(equal(2))
+    expect(folder_count).to(equal(1))
