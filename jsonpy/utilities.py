6acec8cc87d0e6ac69f1e7225fdf6e945d0ee6c2
Harry Peach <harrypeach1999@gmail.com>
2022-09-12 22:58:05 +0100

    Add message when a dlist.zip is provided instead of filelist.json

--- jsonpy/utilities.py
+++ jsonpy/utilities.py
@@ -20,4 +20,8 @@ def check_input_file(input_file: Path) -> bool:
     elif not input_file.exists():
         logging.error("The provided input file did not exist")
         return False
+    elif input_file.name.endswith(".dlist.zip"):
+        logging.error(
+            "You must first extract the filelist.json from the *.dlist.zip and run the"
+            "program on that instead")
     return True
